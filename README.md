7f6ea1b0d9782dc179cc7cb924adef47130359fd
NagapetyanMargarita <mrnagapetyan@mail.ru>
2026-01-07 22:01:05 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Требования
+OC: Ubuntu
+
+Наличие kubeconfig.
+
+- [Docker](https://timeweb.cloud/tutorials/docker/kak-ustanovit-docker-na-ubuntu-22-04)
+- [Kubectl](https://kubernetes.io/ru/docs/tasks/tools/install-kubectl/#%D1%83%D1%81%D1%82%D0%B0%D0%BD%D0%BE%D0%B2%D0%BA%D0%B0-kubectl-%D0%B2-linux)
+- [Helm](https://timeweb.cloud/docs/k8s/helm)
+-  Pip3
+### Запуск
+1. Подставьте собственные значения в файл my_vars.env.
+2. Запустите скрипт
+```
+./run.sh
+```
---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Экспорт переменных
+VARS_FILE="my_vars.env"
+if [[ -f "$VARS_FILE" ]]; then
+  source "$VARS_FILE"
+else
+  echo "Файл $VARS_FILE не найден"
+  exit 1
+fi
+
+# Проверка пользователя
+if [[ "$(id -u)" -ne 0 ]]; then
+  echo "Запуск осуществляется не от root, необходимо ввести пароль."
+  read -s -p "Введите sudo пароль: " ANSIBLE_BECOME_PASS
+  echo
+
+  if [[ -z "$ANSIBLE_BECOME_PASS" ]]; then
+    echo "Пароль не может быть пустым. Запустите скрипт заново."
+    exit 1
+  fi
+  export ANSIBLE_BECOME_PASS
+else
+  echo "Запуск происходит от root — ANSIBLE_BECOME_PASS не требуется"
+fi
+
+mkdir -p logs
+LOG_FILE="logs/$(date +"%Y%m%d_%H%M%S")_installer.log"
+
+# Запуск инсталлятора
+cd ansible && ansible-playbook playbooks/main.yml | tee -a "$LOG_FILE"
